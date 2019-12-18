aed19e4138492fc460f2cddaa0a8e059588b714b
joan parellada <joan.parellada789@gmail.com>
2019-12-18 17:47:40 +0100

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'cocktails#index'
-  get 'cocktails/bootstrap', to: 'cocktails#bootstrap'
-  resources :cocktails, except: :index do
+  root to: 'pages#home'
+  # root to: 'cocktails#index'
+  # get 'cocktails/bootstrap', to: 'cocktails#bootstrap'
+  resources :cocktails do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
